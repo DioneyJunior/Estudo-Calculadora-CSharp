ae5438f2fe0e1936613953c15bcaa558d0ad29c4
Dioney Junior <dioneyfbjunior@gmail.com>
2023-02-03 16:36:24 -0300

    Adicionando textos

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+namespace Calculadora
+{
+    internal class program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("QEscolha a operação desejada")
+                Console.WriteLine("1.Adição")
+                Console.WriteLine("2.Subtração")
+                Console.WriteLine("3.Multiplicação")
+                Console.WriteLine("4.Divisão")
+
+        }
+    }
+}
